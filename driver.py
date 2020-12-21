fdd6f6d3a4d4e1f176ae629a62e748a8e3d99649
erodaly <ross.daly@ericsson.com>
2020-12-21 22:33:05 +0000

    Add driver

---
+++ driver.py
@@ -0,0 +1,11 @@
+from my_draw.board import blank_board
+
+def main():
+    '''
+    Here we will pull together all our modules to create our main driver
+    '''
+    #test board function has been imported
+    blank_board()
+
+if __name__ == "__main__":
+    main()
